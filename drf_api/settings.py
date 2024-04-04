8191243f00e85ed32043a626c432c5b473e8a20b
Dinis Machado <dinisma@gmail.com>
2024-04-04 20:05:17 +0000

    Prepare to deploy

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -95,7 +95,6 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
-    'dj_rest_auth.registration',
     'corsheaders',
     'profiles',
     'posts',

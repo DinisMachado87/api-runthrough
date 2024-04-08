7a1c3761a56f9fdc79bb3d948310a4930bfa3779
Dinis Machado <dinisma@gmail.com>
2024-04-08 22:41:23 +0000

    Add extra COR settings

--- drf_api/views.py
+++ drf_api/views.py
@@ -15,6 +15,7 @@ def root_route(request):
     })
 
 
+# dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
     response = Response()
@@ -22,6 +23,8 @@ def logout_route(request):
         key=JWT_AUTH_COOKIE,
         value='',
         httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
         samesite=JWT_AUTH_SAMESITE,
         secure=JWT_AUTH_SECURE,
     )

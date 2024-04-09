3d9bd1bede518214bf1a66495f6d65575e92fe23
Dinis Machado <dinisma@gmail.com>
2024-04-09 11:20:05 +0000

    reset db

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -49,8 +49,8 @@ if 'DEV' not in os.environ:
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'my_app_auth'
-JWT_AUTH_REFRESH_COOKIE = 'my_app_refresh'
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-app-refresh'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
